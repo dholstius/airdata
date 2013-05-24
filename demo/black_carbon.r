1b33e951a381951dafdae57c5aac05094883a62f
David Holstius <david.holstius@gmail.com>
2013-05-23 18:51:47 -0700

    update black carbon demo

--- demo/black_carbon.r
+++ demo/black_carbon.r
@@ -1,19 +1,23 @@
+# How to find and query the code for a particular pollutant ("black carbon")
 library(airdata)
 library(stringr)
 
-# 1. Find parameter codes corresponding to "black carbon"
-AQDMRS.params <- AQDMRS.list(name="param")
-subset(AQDMRS.list(name="param"), str_detect(tolower(name), "black carbon"))
-
-# 2. Construct a query for a week's worth of hourly data from one site
-BC_data <- AQDMRS.data(
+# If you don't supply user="..." and pw="...", you'll be prompted
+q <- AQDMRS.query(
     state = "06",           # California
     county = "001",         # Alameda County
     site = "0011",          # West Oakland site
-    param = "84313",        # Black carbon (from step #1 above)
     bdate = "2012-04-15",   
     edate = "2012-04-23"
 )
 
+# Find parameter codes corresponding to "black carbon"
+AQDMRS.params <- AQDMRS.list(name="param")
+subset(AQDMRS.list(name="param"), str_detect(tolower(name), "black carbon"))
+
+# Queries are lazy and can be modified before being evaluated
+q$param <- "84313"
+BC_data <- eval(q)
+
 # Plot as a timeseries
-with(BC_data, plot(GMT, Sample.Measurement, main="West Oakland Black Carbon", ylab="ug/m3"))
+with(BC_data, plot(GMT, value, main="West Oakland Black Carbon", ylab="ug/m3"))
